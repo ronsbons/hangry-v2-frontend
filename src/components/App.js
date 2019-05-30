07087d6b4eea7b7acac663dc7bfada310d1995cc
ronsbons <ronnilouie@gmail.com>
2019-05-29 17:00:20 -0700

    started setup of files

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+// import other components
+
+// import './App.css';
+
+class App extends Component {
+  // state
+
+  // component lifecycle methods
+
+  render() {
+    return (
+      <p>hello world</p>
+    );
+  };
+};
+
+export default App;
