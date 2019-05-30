49513f8db626cd595f05450326cef36fd29953fc
ronsbons <ronnilouie@gmail.com>
2019-05-29 17:04:58 -0700

    added axios to App.js to test connection to backend server

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import axios from 'axios';
+
 // import other components
 
 // import './App.css';
@@ -9,6 +11,12 @@ class App extends Component {
   // state
 
   // component lifecycle methods
+  componentDidMount() {
+    // test to connect to backend
+    axios.get('http://localhost:3001/').then(response => {
+      console.log(response.data);
+    });
+  };
 
   render() {
     return (
