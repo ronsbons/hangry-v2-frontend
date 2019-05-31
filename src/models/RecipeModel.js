fdffae6220f4fc02dacd220e3d2ad35a57270178
ronsbons <ronnilouie@gmail.com>
2019-05-30 17:57:36 -0700

    included external API endpoint

--- src/models/RecipeModel.js
+++ src/models/RecipeModel.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 // [] endpoint will have to be atlas hosted mongodb
-const endPoint = '';
+const endPoint = 'https://api.edamam.com/search?';
+const appId = 'ed9e50d2';
+const appKey = 'b5858adbdef15d7fa7b69b92cebe450e';
 
 class RecipeModel {
-  static getAllRecipes(){
-    let request = axios.get(`${endPoint}`);
+  static searchRecipes(query){
+    let request = axios.get(`${endPoint}q=${query}&app_id=${appId}&app_key=${appKey}&from=0&to=6`);
     return request;
   };
 };
