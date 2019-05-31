5fd1227fcdb0af3ca6ef3d3ffc741a3dc53e138c
ronsbons <ronnilouie@gmail.com>
2019-05-30 18:41:01 -0700

    changed app id and app key to env variables

--- src/models/RecipeModel.js
+++ src/models/RecipeModel.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // [] endpoint will have to be atlas hosted mongodb
 const endPoint = 'https://api.edamam.com/search?';
-const appId = 'ed9e50d2';
-const appKey = 'b5858adbdef15d7fa7b69b92cebe450e';
+const appId = process.env.REACT_APP_APP_ID;
+const appKey = process.env.REACT_APP_APP_KEY;
 
 class RecipeModel {
   static searchRecipes(query){
