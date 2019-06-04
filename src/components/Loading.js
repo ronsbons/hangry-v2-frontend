d57f42aeb2a730ea4340586c1a6e1ef0fb70e209
ronsbons <ronnilouie@gmail.com>
2019-06-03 18:29:21 -0700

    added Loading component and logic to show during axios call

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// stateless functional component
+const Loading = () => (
+  <div className="loading">
+    <i className="fa fa-spinner fa-spin" />Loading ...
+  </div>
+);
+
+export default Loading;
